5e02dfd0aef66dc7d4df46da4f5bc5752d740f8c
Felipe Bueno <felipebueno2201@gmail.com>
2022-12-21 03:58:58 -0300

    fix: jet error

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,6 +5,7 @@ export async function authMiddleware(req, res, next) {
   const { authorization } = req.headers;
   const token = authorization?.replace("Bearer ", "");
   let id = 0;
+  let err;
 
   if (!token) {
     return res.sendStatus(401);
@@ -12,11 +13,14 @@ export async function authMiddleware(req, res, next) {
 
   jwt.verify(token, process.env.SECRET_JWT, (error, decoded) => {
     if (error) {
-      return res.status(401).send({ message: "Invalid Token!" });
+      err = error;
+    } else {
+      id = decoded.id;
     }
-    id = decoded.id;
   });
-
+  if (err) {
+    return res.status(401).send({ message: "Invalid Token!" });
+  }
   try {
     const user = await connection.query(
       `SELECT * FROM users WHERE users.id = '${id}'`
@@ -26,7 +30,6 @@ export async function authMiddleware(req, res, next) {
     }
     delete user.rows[0].password;
     delete user.rows[0].confirmPassword;
-    console.log(user.rows[0]);
     req.user = user.rows[0];
     next();
   } catch (err) {
